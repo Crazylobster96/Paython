f5355343e1878c38f441a5d8649c7ad8001c6b2a
Crazylobster96 <crazylobster96@gmail.com>
2015-02-07 00:49:38 +0800

    logmessage
    add sys time into log message
    & add error to message

--- Py_broadcast/pybrct.py
+++ Py_broadcast/pybrct.py
@@ -11,8 +11,26 @@ MessageBox = []
 host = ''
 port = '51423'
 
-def writelog(log):
+def write_erro(errstr):
     global lglock, msglock,logcount, LogMessage, MessageBox
+    logtime=time.strftime('%Y-%m-%d-%H-%M-%S',time.localtime(time.time()))
+    log = logtime+' Error:  '+log
+    lglock.acquire()
+    LogMessage.append(log)
+    logcount += 1
+    f_log = open('log.txt','a')
+    while (logcount > 0):
+            f_log.write(LogMessage[0])
+            f_log.write('\n')
+            LogMessage.remove(LogMessage[0])
+            logcount -= 1
+        f_log.close()
+    lglock.release()
+    
+def write_log(log):
+    global lglock, msglock,logcount, LogMessage, MessageBox
+    logtime=time.strftime('%Y-%m-%d-%H-%M-%S',time.localtime(time.time()))
+    log = logtime+' Log:  '+log
     lglock.acquire()
     LogMessage.append(log)
     logcount += 1
@@ -25,7 +43,8 @@ def writelog(log):
             logcount -= 1
         f_log.close()
     lglock.release()
-def logclose():
+    
+def Sync_log():
     global lglock, msglock,logcount, LogMessage, MessageBox
     lglock.acquire()
     if(logcount > 0):
@@ -44,10 +63,16 @@ def BroadCast(b_message,timeout):
     broadcast_s.setsockopt(socket.SOL_SOCKET,socket.SO_BROADCAST,1)
     broadcast_s.sendto(b_message,dest)
 
+def BrdC_Recv():
+    
 def Main_thread():
     global lglock, msglock,logcount, LogMessage, MessageBox
-    writelog('Main thread start!')
-    logclose()
+    write_log('Main thread start!')
+    #start recv thread;
+    
+    #broadcast self
+    #join thread recv;
+    Sync_log()
      
 def main():
     Main_thread()
